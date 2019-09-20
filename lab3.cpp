6c4b6fa38ec958e798697924b772273c8b0fe172
Ryan McHale <mrryanmchale@outlook.com>
2019-09-20 13:01:02 -0400

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -26,33 +26,15 @@ constexpr float pi = 3.14159265;
 enum wall { left = -1, right = 1 };
 
 constexpr wall follow = wall::right; // for when zane decides the robot should go the other way...
-constexpr double setpoint = pi - pi/2 * follow;
-constexpr double setdist = .25;
-
-/*********************************/
-/*         program logic         */
-/*********************************/
-vector<float> lidar_data;
-float angle_increment;
-float angle_min;
-float angle_max;
-/**
- * Loads data from lidar and stores it in global variables
- */
-static void processLaserScan(sensor_msgs::LaserScan::ConstPtr const &scan)
-{
-    lidar_data = scan->ranges;
-    angle_increment = scan->angle_increment;
-    angle_min = scan->angle_min;
-    angle_max = scan->angle_max;
-}
+constexpr float setpoint = pi - pi/2 * follow;
+constexpr float setdist = .25;
 
 /**
  * returns the difference between two angles
  * Ex:
  *      pi/2 = rel_angle(3*pi/2, pi);
  */
-constexpr double rel_angle(double const& angle, double const& reference)
+constexpr auto rel_angle(auto const& angle, auto const& reference)
 {
     return atan2(sin(reference-angle), cos(reference-angle));
 }
@@ -62,32 +44,37 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "lab3");
 
     ros::NodeHandle n;
+
     ros::Publisher cmd_vel = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
-    ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 10, &processLaserScan);
+
+    sensor_msgs::LaserScan::ConstPtr lidar;
+    ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 10, [&lidar](auto const& scan){
+        lidar = scan;
+    });
 
     // This is initialized outside of the loop because ros is bad at initializing things efficiently
     geometry_msgs::Twist twist;
     
     while (ros::ok())
     {
-        if(!lidar_data.empty()) // it's empty for the first few iterations for some reason...
+        if(! lidar->ranges.empty() ) // it's empty for the first few iterations for some reason...
         {
             // find wall angle
-            auto const& min_reading = std::min_element(lidar_data.begin(), lidar_data.end());
+            auto const& min_reading = std::min_element(begin(lidar->ranges), end(lidar->ranges));
             int const min_pos = min_reading - lidar_data.begin();
 
-            double const wall_angle = angle_min + angle_increment * min_pos;
+            float const wall_angle = lidar->angle_min + lidar->angle_increment * lidar->min_pos;
 
             // calculate correction angle
-            double const yaw_err = rel_angle(setpoint, wall_angle);
+            float const yaw_err = rel_angle(setpoint, wall_angle);
 
             // tendancy to move to a set distance from the wall
-            double const y_err = (*min_reading - setdist) * follow;
-            double const find_distance = y_err*y_err*y_err*atan(-y_err) * sin(wall_angle);
+            float const y_err = (*min_reading - setdist) * follow;
+            float const find_distance_correction = y_err*y_err*y_err*atan(-y_err) * sin(wall_angle);
 
             // set ouputs
             twist.linear.x = .5;
-            twist.angular.z = 10*yaw_err + 8*find_distance;
+            twist.angular.z = 10*yaw_err + 8*find_distance_correction;
 
             cmd_vel.publish(twist);
         }
