bdca926e810fc3235911ae9598bcbf4920d044d2
Ryan McHale <mrryanmchale@outlook.com>
2019-09-21 22:44:31 -0400

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -24,7 +24,7 @@ using namespace geometry_msgs;
 constexpr float pi = 3.14159265;
 constexpr float min_range = .05;
 
-enum wall { left = -1, right = 1 };
+enum wall { left = 1, right = -1 };
 
 constexpr wall follow = wall::right; // for when zane decides the robot should go the other way...
 constexpr auto setpoint = pi - pi/2 * follow;
