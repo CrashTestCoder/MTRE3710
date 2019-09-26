467bc0883758ec3d6061b54067b6ceb5b40f6cc5
Ryan McHale <mrryanmchale@outlook.com>
2019-09-26 17:05:31 -0400

    remove getch

--- lab3.cpp
+++ lab3.cpp
@@ -44,35 +44,6 @@ constexpr double setdist = .3; // .27
 constexpr double setdist = .3; // .27
 #endif
 
-// For non-blocking keyboard inputs
-int getch(void)
-{
-	int ch;
-	struct termios oldt;
-	struct termios newt;
-
-	// Store old settings, and copy to new settings
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-
-	// Make required changes and apply the settings
-	newt.c_lflag &= ~(ICANON | ECHO);
-	newt.c_iflag |= IGNBRK;
-	newt.c_iflag &= ~(INLCR | ICRNL | IXON | IXOFF);
-	newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
-	newt.c_cc[VMIN] = 0;
-	newt.c_cc[VTIME] = .000001;
-	tcsetattr(fileno(stdin), TCSANOW, &newt);
-
-	// Get the current character
-	ch = getchar();
-
-	// Reapply old settings
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-	return ch;
-}
-
 /**
  * returns the difference between two angles
  * Ex:
