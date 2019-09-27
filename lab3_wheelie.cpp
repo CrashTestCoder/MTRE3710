5f9ff9b3cc2799784d1f75a0741088dcb3c1e314
Ryan McHale <mrryanmchale@outlook.com>
2019-09-27 12:19:10 -0400

    Update lab3_wheelie.cpp

--- lab3_wheelie.cpp
+++ lab3_wheelie.cpp
@@ -31,13 +31,32 @@ enum wall { left = 1, right = -1 };
 constexpr wall follow = wall::right; // for when zane decides the robot should go the other way...
 constexpr float setpoint = pi - pi/2 * follow;
 constexpr float setdist = .25;
-constexpr float speed = .25;
+constexpr float def_speed = .25;
+float speed = def_speed;
 
 
 
 /*********************************/
 /*         program logic         */
 /*********************************/
+
+void start_wheelie()
+{
+    twist = geometry_msgs::Twist();
+
+    twist.linear.x = -1;
+    cmd_vel.publish(twist);
+
+    ros::Duration(.1).sleep();
+
+    twist.linear.x = 1;
+    cmd_vel.publish(twist);
+
+    ros::Duration(.1).sleep();
+    
+    twist.linear.x = .5;
+    cmd_vel.publish(twist);
+}
 
 constexpr float get_wheelie_angle(float const dist) noexcept
 {
@@ -54,11 +73,50 @@ constexpr float get_wheelie_angle(float const dist) noexcept
     return theta.real;
 }
 
+static void correct_pitch(auto const current, auto const target)
+{
+    if(curent < .2) start_wheelie();
+
+    twist.linear.x = target - current + speed;
+}
+
 geometry_msgs::Twist twist;
 ros::Publisher cmd_vel;
 static void processLaserScan(sensor_msgs::LaserScan::ConstPtr const &scan)
 {
-    
+    // wheelie
+    auto const& back = find_if_not(begin(scan->ranges) + scan->ranges.size()/2, end(scan->ranges),
+        [](auto const& data){
+            return data < min_range;
+        });
+
+    float const robot_pitch = get_wheelie_angle(*back);
+
+    correct_pitch(robot_pitch, pi/3);
+
+    // wall follow
+
+    int fr_size = 10;
+    decltype(back) fr_begin, fr_end; // follow range
+    if(follow == right) {
+        fr_begin = begin(scan->ranges) + scan->ranges.size()/4 - fr_size/2;
+        fr_end = fr_begin + fr_size;
+    } else {
+        fr_begin = begin(scan->ranges) + 3*scan->ranges.size()/4 - fr_size/2;
+        fr_end = fr_begin + fr_size;
+    }
+
+    auto const& min_dist = min_element(fr_begin, fr_end);
+    if((min_dist == fr_begin || min_dist == fr_end) && speed > 0)
+        speed -= .01;
+    else if(speed < def_speed)
+        speed += .01;
+
+    float const wall_angle = scan->angle_increment * ((int)min_dist - fr_size / 2);
+
+    twist.angular.z = wall_angle * follow;
+
+    cmd_vel.publish(twist);
 }
 
 int main(int argc, char **argv)
